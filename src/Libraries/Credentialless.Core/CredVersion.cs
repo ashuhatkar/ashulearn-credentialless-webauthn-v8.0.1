38f8a188ffe1a1f53cb072449ec675961f2d16d7
Ashish <ashish.hatkar@gmail.com>
2023-12-11 20:26:07 +0530

    Application version

---
+++ src/Libraries/Credentialless.Core/CredVersion.cs
@@ -0,0 +1,23 @@
+namespace Credentialless.Core
+{
+    /// <summary>
+    /// Represents credentialless version
+    /// </summary>
+    public static class CredVersion
+    {
+        /// <summary>
+        /// Gets or sets major version
+        /// </summary>
+        public const string CURRENT_VERSION = "4.60";
+
+        /// <summary>
+        /// Gets or sets minor version
+        /// </summary>
+        public const string MINOR_VERSION = "5";
+
+        /// <summary>
+        /// Gets or sets full version
+        /// </summary>
+        public const string FULL_VERSION = CURRENT_VERSION + "." + MINOR_VERSION;
+    }
+}
